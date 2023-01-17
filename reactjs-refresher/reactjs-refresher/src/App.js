4e6109f4bab8d512b485270096500da19a085eb6
Felipe Kuhne <felipe.kuhne@hp.com>
2023-01-16 23:31:43 -0300

    refresher done.

--- reactjs-refresher/reactjs-refresher/src/App.js
+++ reactjs-refresher/reactjs-refresher/src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 
 const App = () => {
 
-  return <div>
-    <h2>Course Goals</h2>
-    <ul>
-      <li>Finish the course</li>
-      <li>Learn all about the Course Main Topic</li>
-      <li>Help other students in the Course Q&A</li>
-    </ul>
-  </div>
+  const [courseGoals, setCourseGoals] = useState([
+    {id: 'cg1', text: 'Finish the course'},
+    {id: 'cg2', text: 'Learn all about the Course Main Topic'},
+    {id: 'cg3', text: 'Help other students in the Course Q&A'},
+  ]);
 
+  const addNewGoalHandler = newGoal => {
+    // Not so good because can suffer from concurrent calls
+    //setCourseGoals(courseGoals.concat(newGoal));
+
+    // the best option in order to deal with concurrency is:
+    setCourseGoals((prevCourseGoals) => {
+      return prevCourseGoals.concat(newGoal);
+    });
+  }
+
+  return (
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler}/>
+      <GoalList goals={courseGoals}/>
+    </div>
+  )
 }
 
 export default App;
